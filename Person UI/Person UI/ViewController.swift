5818e05d94db28fafdb0995227e7a8644f8f608d
djwwx7 <djwwx7@mail.missouri.edu>
2017-06-30 11:57:43 -0500

    First commit of project.

---
+++ Person UI/Person UI/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Person UI
+//
+//  Created by Whitaker, Cody (MU-Student) on 6/30/17.
+//  Copyright © 2017 Whitaker, Cody (MU-Student). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
